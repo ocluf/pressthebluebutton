57940d951ccd8392f5c7b13d89fa2c447f8bf78d
ocluf <fulco.taen@gmail.com>
2024-01-09 13:19:09 +0100

    x

--- src/routes/red/+page.svelte
+++ src/routes/red/+page.svelte
@@ -1,23 +1,22 @@
 <div class="bg-gray-950 text-gray-100 h-screen p-10">
 	<div class="max-w-[60ch] mx-auto text-xl">
 		<h1 class="text-5xl mt-10">
-			You pressed the <span
-				class="text-red-500
-    ">red</span
-			> button
+			You pressed the <span class="text-red-500">red</span> button
 		</h1>
 
 		<p class="mt-10">
-			Apologies I really thought I couldn't make it any clearer that you were supposed to press the
-			blue button. Next time I'll try to make it extra clear. Maybe a big flashing arrow to the blue
-			button would help?
+			Apologies, I really thought I couldn't make it any clearer that you were supposed to press the
+			blue button. Next time, I'll try to make it extra clear. Maybe a big flashing arrow pointing
+			to the blue button would help?
 		</p>
 
 		<p class="mt-10">
-			It's a bit of a bummer. I had fun little stats about the button presses to show you. if only
-			you had pressed that <span class="text-blue-500">blue</span> button!
+			It's a bit of a bummer. Maybe you that the presses would be different, but equally rewarding?
+			That is definitely not the case. I have fun little dynamic stats about the button presses for
+			people who pressed the <span class="text-blue-500">blue</span> button. and they can freely view
+			this screen as well.
 		</p>
 
-		<p class="mt-10">Well better luck next time.</p>
+		<p class="mt-10">Oh well, better luck next time!</p>
 	</div>
 </div>
